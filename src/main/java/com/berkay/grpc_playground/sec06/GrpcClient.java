54b410333b870b3476f11e470c526c11cb2e74b6
Berkay Simsek <abidinberkay@gmail.com>
2025-01-06 20:55:17 +0300

    async stub

--- src/main/java/com/berkay/grpc_playground/sec06/GrpcClient.java
+++ src/main/java/com/berkay/grpc_playground/sec06/GrpcClient.java
@@ -1,8 +1,10 @@
 package com.berkay.grpc_playground.sec06;
 
+import com.berkay.models.sec06.AccountBalance;
 import com.berkay.models.sec06.BalanceCheckRequest;
 import com.berkay.models.sec06.BankServiceGrpc;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,16 +12,44 @@ public class GrpcClient {
 
     private static final Logger logger = LoggerFactory.getLogger(GrpcClient.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         var channel = ManagedChannelBuilder.forAddress("localhost", 6565)
                 .usePlaintext()
                 .build();
 
-        var stub = BankServiceGrpc.newBlockingStub(channel);
+        // BLOCKING STUB
+        /*var stub = BankServiceGrpc.newBlockingStub(channel);
 
         var balance = stub.getAccountBalance(BalanceCheckRequest.newBuilder().setAccountNumber(2).build());
         logger.info("{}", balance);
+        */
+        // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+        //ASYNC STUB
+
+        var stub = BankServiceGrpc.newStub(channel);
+
+        stub.getAccountBalance(BalanceCheckRequest.newBuilder().setAccountNumber(2).build(), new StreamObserver<AccountBalance>() {
+            @Override
+            public void onNext(AccountBalance accountBalance) {
+                logger.info("{}", accountBalance);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                logger.info("Completed");
+            }
+        });
+
+        logger.info("done");
+        Thread.sleep(2000);
+
     }
 
 }
